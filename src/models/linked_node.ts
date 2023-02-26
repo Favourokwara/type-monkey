62804dc1745c841cc95db839423ccef90c7b3f0f
Favour Okwara <favourprogrammer@gmail.com>
2023-02-26 02:05:58 +0200

    Add LinkedNode and DoublyNode data class

--- src/models/linked_node.ts
+++ src/models/linked_node.ts
@@ -4,3 +4,13 @@ export interface ListNode { next?: ListNode, getValue(): any };
 // interface describing properties of a doubly linked list item
 export interface DoublyListNode extends ListNode {
     prev?: DoublyListNode, next?: DoublyListNode };
+
+export class LinkedNode implements ListNode {
+    constructor(private value: any, public next?: LinkedNode){}
+    getValue(): any { return this.value; }
+};
+
+export class DoublyNode extends LinkedNode implements DoublyListNode {
+    constructor(value: any, public prev?: DoublyNode, next?: DoublyNode)
+    { super(value, next) }
+}
