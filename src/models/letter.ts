062ea097261dfaf73011b4eccfc4a7eff1566587
Favour Okwara <favourprogrammer@gmail.com>
2023-02-27 21:57:30 +0200

    Create Letter data class that implements LetterNode interface

--- src/models/letter.ts
+++ src/models/letter.ts
@@ -1,1 +1,5 @@
 export interface LetterNode { character: string, letterIndex: number, wordIndex: number, wordLength: number, }
+
+export class Letter implements LetterNode { 
+    constructor(public character: string, public letterIndex: number, public wordIndex: number, public wordLength: number) {}
+}
