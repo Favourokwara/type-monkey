7b718e6b85ec2429aa8fcb4c22d2322ec018e57b
Favourokwara <favourprogrammer@gmail.com>
2023-02-28 17:25:24 +0200

    Add Letter interface description

--- src/models/letter.ts
+++ src/models/letter.ts
@@ -1,3 +1,4 @@
+// Interface describing a single letter within a sentence
 export interface LetterNode { character: string, letterIndex: number, wordIndex: number, wordLength: number, }
 
 export class Letter implements LetterNode { 
