079c19bd4c1e05776f34f74569c5c9f8bf5bd0fd
Favour Okwara <faokwar022@student.wethinkcode.co.za>
2023-02-28 14:56:04 +0200

    Add interface description

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,75 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-import confetti from 'canvas-confetti';
+import { Letter } from "./models/letter";
+import { DoublyLinkedList } from "./models/linked_list";
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+// import confetti from 'canvas-confetti';
+
+// confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
+//   resize: true,
+//   useWorker: true,
+// })({ particleCount: 200, spread: 200 });
+
+function sentenceToDoublyLinkedList(sentence: string): DoublyLinkedList {
+  const words: string[] = sentence.split(/(?<=\s)/);
+  const doublyList = new DoublyLinkedList();
+
+  for (let i = 0; i < words.length; i++) {
+    const wordLength = words[i].length;
+    for (let j = 0; j < words[i].length; j++) {
+      doublyList.push(new Letter(words[i][j], j, i, wordLength));
+    }
+  } 
+  return doublyList
+}
+
+
+class SentenceService {
+  public reference: DoublyLinkedList;
+  public typed_lst: DoublyLinkedList = new DoublyLinkedList();
+
+  constructor(ref: string) {
+    this.reference = sentenceToDoublyLinkedList(ref);
+  }
+  addCharacter(str: string) {
+    if (str.length == 1 && this.typed_lst.size < this.reference.size) {
+      const base: Letter = this.reference.getElementAt(this.typed_lst.size)?.getValue();
+      base.character = str;
+      this.typed_lst.push(base)
+    }
+  }
+  removeCharacter(): Letter {
+    return this.typed_lst.removeAt(this.typed_lst.size - 1);
+  }
+  removeWord(): Letter {
+    const tail = this.typed_lst.tail;
+    let current = tail
+    const moves = current?.getValue().letterIndex;
+    for (let i = 0; i < moves.length + 1 ; i++) {
+      current = current?.prev;
+    }
+    console.log(current);
+    
+  }
+}
+
+const service = new SentenceService("this is what i do for a living.")
+service.addCharacter("t")
+service.addCharacter("h")
+service.addCharacter("i")
+service.addCharacter("s")
+service.addCharacter(" ")
+service.addCharacter("i")
+service.addCharacter("s")
+service.addCharacter(" ")
+service.addCharacter("w")
+service.addCharacter("h")
+service.addCharacter("a")
+service.addCharacter("t")
+service.addCharacter(" ")
+service.addCharacter("i")
+
+// console.log(service.removeWord())
+console.log(service.typed_lst);
+
