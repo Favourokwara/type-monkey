d2bd3fb7c205bd378b98778b33883ed5a631e744
Favourokwara <favourprogrammer@gmail.com>
2023-03-01 14:27:59 +0200

    Add docstrings for the Letter data class

--- src/models/letter.ts
+++ src/models/letter.ts
@@ -1,6 +1,7 @@
 // Interface describing a single letter within a sentence
 export interface LetterNode { character: string, letterIndex: number, wordIndex: number, wordLength: number, }
 
+/** Class used to represent letters within a sentence or character sequence. */
 export class Letter implements LetterNode { 
     constructor(public character: string, public letterIndex: number, public wordIndex: number, public wordLength: number) {}
 }
