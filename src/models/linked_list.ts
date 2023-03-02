0099357b959c0477a95820b254981129e9fb8aee
Favour Okwara <favourprogrammer@gmail.com>
2023-03-02 20:41:43 +0200

    Refactor Singly Linked List class

--- src/models/linked_list.ts
+++ src/models/linked_list.ts
@@ -7,3 +7,30 @@ export interface SinglyList { head?: SinglyLinkedNode, getSize(): number,
     getElementAt(index: number): SinglyLinkedNode | undefined,
     removeAt(index: number): any, remove(item: any): any, isEmpty(): boolean,
 }
+
+function defaultEquals(a: any, b: any): boolean { return a === b; }
+
+export class SinglyLinkedList implements SinglyList {
+    private size: number = 0;
+    public head?: SinglyLinkedNode;
+    constructor(private equalsFn: Function = defaultEquals) {}
+
+    /**Returns the size of the linked list.*/
+    getSize(): number { return this.size; }
+
+    /**Appends the provided item to the end of the linked list. */
+    push(item: any): void {
+        const node = new SinglyLinkedNode(item);
+
+        if (this.head == undefined) {
+            this.head = node;
+        } else {
+            let current = this.head;
+
+            // find the end of the linked list and append item to it
+            while (current?.next != null) { current = current.next;}
+            current.next = node;
+        }
+        this.size++;
+    }
+}
