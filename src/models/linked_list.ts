662dd9c89fc97f78ec36e9b02d70230bcc3df59f
Favour Okwara <favourprogrammer@gmail.com>
2023-03-03 21:17:09 +0200

    Expand singly linked list interface

--- src/models/linked_list.ts
+++ src/models/linked_list.ts
@@ -2,10 +2,16 @@
 import { SinglyLinkedNode, DoublyLinkedNode } from "./linked_node";
 
 /**Interface that desribes the singly linked list data structure. */
-export interface SinglyList { head?: SinglyLinkedNode, getSize(): number,
-    push(item: any): void, insert(item: any, index: number): boolean,
-    getElementAt(index: number): SinglyLinkedNode | undefined,
-    removeAt(index: number): any, remove(item: any): any, isEmpty(): boolean,
+export interface SinglyList {
+    head?: SinglyLinkedNode,
+    getSize(): number,
+    push(item: any): void,
+    insert(item: any, index: number): boolean,
+    getElementAt(index: number): SinglyLinkedNode | undefined, 
+    removeAt(index: number): any,
+    remove(item: any): any,
+    indexOf(item: any): number,
+    isEmpty(): boolean,
 }
 
 function defaultEquals(a: any, b: any): boolean { return a === b; }
