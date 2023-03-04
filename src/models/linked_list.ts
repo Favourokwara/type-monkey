46f01d3166972d84b1d9ae3d6048b7470f9d77be
Favour Okwara <favourprogrammer@gmail.com>
2023-03-04 10:09:46 +0200

    Format singly linked list class

--- src/models/linked_list.ts
+++ src/models/linked_list.ts
@@ -1,113 +1,129 @@
-// Import all items required to build the linked list data structure.
-import { SinglyLinkedNode, DoublyLinkedNode } from "./linked_node";
+import { SinglyLinkedNode, DoublyLinkedNode } from './linked_node';
 
-/**Interface that desribes the singly linked list data structure. */
+/**Interface that describes the behaviour of the linked list data structure. */
 export interface SinglyList {
-    head?: SinglyLinkedNode,
-    getSize(): number,
-    push(item: any): void,
-    insert(item: any, index: number): boolean,
-    getElementAt(index: number): SinglyLinkedNode | undefined, 
-    removeAt(index: number): any,
-    remove(item: any): any,
-    indexOf(item: any): number,
-    isEmpty(): boolean,
+  head?: SinglyLinkedNode;
+  getSize(): number;
+  push(item: any): void;
+  insert(item: any, index: number): boolean;
+  remove(item: any): void;
+  removeAt(index: number): any;
+  indexOf(item: any): number;
+  isEmpty(): boolean;
+  getElementAt(index: number): SinglyLinkedNode | undefined;
 }
 
-function defaultEquals(a: any, b: any): boolean { return a === b; }
+/**Interface that describes the behaviour of the doubly list data structure. */
+export interface DoublyList extends SinglyList {
+  head: DoublyLinkedNode;
+  getElementAt(index: number): DoublyLinkedNode | undefined;
+}
 
-/**Class that represents the Singly Linked List data structure. */
+function defaultEquals(a: any, b: any): boolean {
+  return a === b;
+}
+
 export class SinglyLinkedList implements SinglyList {
-    private size: number = 0;
-    public head?: SinglyLinkedNode;
-    constructor(private equalsFn: Function = defaultEquals) {}
+  // Define the linked list's state properties
+  private size: number = 0;
+  public head?: SinglyLinkedNode = undefined;
+  // Get function to compare list item values.
+  constructor(private equalsFn: Function = defaultEquals) {}
 
-    /**Returns the number of items within the linked list object. */
-    getSize(): number { return this.size; }
+  /**Returns the number of items in the singly linked list data structure. */
+  getSize(): number {
+    return this.size;
+  }
 
-    /**Removes the item in the linked list with the provided index.*/
-    remove(item: any) { return this.removeAt(this.indexOf(item)); }
+  /**Remove the item in the list that mathces the provided list item value.*/
+  remove(item: any) {
+    return this.removeAt(this.indexOf(item));
+  }
 
-    /**Returns boolean indicating whether or not the list is empty.*/
-    isEmpty(): boolean { return this.head == undefined; }
+  /**Returns a boolean indicating whether or not the linked list is empty. */
+  isEmpty(): boolean {
+    return this.head === undefined;
+  }
 
-    /**Appends the specified element to the end of the linked list.*/
-    push(item: any): void {
-        let node = new SinglyLinkedNode(item), current = this.head;
-
-        if (current === undefined) { this.head = node; }
-        else {
-            // get the last item within the list and ensure that its
-            // link is pointed towards the newly created SinglyNode.
-            while (current?.next != null) { current = current.next; }
-            current.next = node;
-        }
-        this.size++;
+  /**Appends the specified item to the last node of the of the linked list.*/
+  push(item: any): void {
+    // Create new instance of the singly linked node
+    const node = new SinglyLinkedNode(item);
+    let current = this.head;
+    // Check whether the linked list is empty or not
+    if (current == null) {
+      this.head = node;
+    } else {
+      // Assign the last items's pointer to the newly created singly node
+      while (current?.next != null) {
+        current = current.next;
+      }
+      current.next = node;
     }
+    this.size++;
+  }
 
-    /**Inserts the provided item into the specified index position.*/
-    insert(item: any, index: number): boolean {
-        // checks if the provided index is within the allowed ranges
-        if (index >= 0 && index <= this.size) {
-            const node = new SinglyLinkedNode(item);
+  /**Inserts the provided item to the end of the specified index position. */
+  insert(item: any, index: number): boolean {
+    // Check whether or not the specified index position is within range
+    if (index >= 0 && index <= this.size) {
+      // Create a new instance of the singly linked node
+      const node = new SinglyLinkedNode(item);
 
-            if (index === 0) {
-                node.next = this.head;
-                this.head = node;
-            } else {
-                const previous = this.getElementAt(index - 1);
-                let current = this.head;
-                if (previous) {
-                    current = previous.next;
-                    node.next = current;
-                    previous.next = node;
-                }
-            }
-            this.size++;
-            return true;
+      if (index === 0) {
+        (node.next = this.head), (this.head = node);
+      } else {
+        // Get the item before the given index and change its pointers
+        // to point towards the newly created node element.
+        let prev = this.getElementAt(index - 1),
+          curr = prev?.next;
+        if (prev) {
+          (node.next = curr), (prev.next = node);
         }
-        return false;
+      }
     }
+    return false;
+  }
 
-    /**Returns `SinglyLinkedNode` at the specified index position. */
-    getElementAt(index: number): SinglyLinkedNode | undefined {
-        if (index >= 0 && index < this.size) {
-            let current = this.head;
+  /**Returns the singly linked node item at the specified index position. */
+  getElementAt(index: number): SinglyLinkedNode | undefined {
+    // Check whether or not the specified index position is within range
+    if (index >= 0 && index < this.size) {
+      let current = this.head;
 
-            for (let i = 0; i < index && current != null; i++)
-            current = current.next;
+      for (let i = 0; i < index && current != null; i++) current = current.next;
+      return current;
+    }
+  }
 
-            return current;
+  /**Removes the item in the linked list at the specified index position. */
+  removeAt(index: number) {
+    // Check whether or not the specified index position is within range
+    if (index >= 0 && index < this.size) {
+      let current = this.head;
+      // Check whether we're removing the first item in the linked list
+      if (index === 0) {
+        this.head = current?.next;
+      } else {
+        const prev = this.getElementAt(index - 1);
+        if (prev) {
+          (current = prev.next), (prev.next = current?.next);
         }
+      }
+      this.size--;
+      return current?.getValue();
     }
+  }
 
-    /**Removes the item in the linked list at the specified index. */
-    removeAt(index: number) {
-        if (index >= 0 && index < this.size) {
-            let current = this.head;
-
-            if (index === 0) this.head = current?.next;
-            else {
-                const previous = this.getElementAt(index -1);
-
-                if (previous) {
-                    current = previous.next;
-                    previous.next = current?.next;
-                }
-            }
-            this.size--;
-            return current?.getValue();
-        }
+  /**Returns the index if the specified item value inside the linked list. */
+  indexOf(item: any): number {
+    let current = this.head;
+    for (let i = 0; i < this.size; i++) {
+      if (this.equalsFn(item, current?.getValue())) {
+        return i;
+      }
+      current = current?.next;
     }
-
-    /**Returns the index of the specified item in the linked list. */
-    indexOf(item: any): number {
-        let current = this.head;
-
-        for (let i = 0; i < this.size; i++) {
-            if (this.equalsFn(item, current?.getValue())) return i;
-            current = current?.next;
-        }
-        return -1;
-    }
+    return -1;
+  }
 }
