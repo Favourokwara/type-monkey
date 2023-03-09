b6e0cf95480f1eeb01f863ada27e4ee8c5ea5b08
Favour Okwara <favourprogrammer@gmail.com>
2023-03-09 20:24:48 +0200

    add doubly node class

--- src/models/node.ts
+++ src/models/node.ts
@@ -5,3 +5,9 @@ export class SinglyNode implements SinglyLinkedNode<any> {
   public next?: SinglyLinkedNode<any>;
   constructor(public value: any) {}
 }
+
+/**Data class that's used to build up and store data in doubly linked lists. */
+export class DoublyNode extends SinglyNode implements SinglyLinkedNode<any> {
+  public declare next?: DoublyLinkedNode<any>;
+  public declare prev?: DoublyLinkedNode<any>;
+}
