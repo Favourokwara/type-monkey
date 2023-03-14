ab70b6c0d364b08a67fbd52fe00cf8393a439a87
Favour Okwara <favourprogrammer@gmail.com>
2023-03-14 16:06:39 +0200

    implement linked node data class

--- src/models/linked_node.ts
+++ src/models/linked_node.ts
@@ -1,31 +1,13 @@
-/**Interface that describes nodes required to build a signly linked list.*/
-export interface SinglyNode {
-  next?: SinglyNode;
-  getValue(): any;
+import { SinglyLinkedNode, DoublyLinkedNode } from '../interfaces/linked_nodes';
+
+/**Data class used for building up and storing data in a singly linked list. */
+export class SinglyNode implements SinglyLinkedNode {
+  constructor(public value: any, public next?: SinglyNode) {}
 }
 
-/**Interface that describes nodes required to build a doubly linked list.*/
-export interface DoublyNode extends SinglyNode {
-  next?: DoublyNode;
-  prev?: DoublyNode;
-}
-
-/**Data class used to build up and store data in a linked list data structure.*/
-export class SinglyLinkedNode implements SinglyNode {
-  constructor(private _data: any, public next?: SinglyLinkedNode) {}
-  /**Returns the data that is stored within the linked list node.*/
-  getValue() {
-    return this._data;
-  }
-}
-
-/**Data class used to build up and store data within a doubly linked list.*/
-export class DoublyLinkedNode extends SinglyLinkedNode implements DoublyNode {
-  constructor(
-    _data: any,
-    public next?: DoublyLinkedNode,
-    public prev?: DoublyLinkedNode,
-  ) {
-    super(_data, next);
+/**Data class used for building up and storing data in a doubly linked list. */
+export class DoublyNode extends SinglyNode implements DoublyLinkedNode {
+  constructor(value: any, next?: DoublyNode, public prev?: DoublyNode) {
+    super(value, next);
   }
 }
