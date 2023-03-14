1cbb591885dd704243d47762bfe189ccb422eea2
Favour Okwara <favourprogrammer@gmail.com>
2023-03-14 12:47:43 +0200

    remove node type annotation

--- src/interfaces/linked_nodes.ts
+++ src/interfaces/linked_nodes.ts
@@ -1,20 +1,20 @@
 /**Abstract class that describes the behavior of a singly linked list node. */
-export interface SinglyLinkedNode<Type> {
+export interface SinglyLinkedNode {
   /**The next value attached to the linked list */
-  next?: SinglyLinkedNode<Type>;
+  next?: SinglyLinkedNode;
 
   /**Stores the values and data inside the list */
-  value: Type;
+  value: any;
 }
 
 /**Abstract class that describes the behavior of a doubly linked list node. */
-export interface DoublyLinkedNode<Type> extends SinglyLinkedNode<Type> {
+export interface DoublyLinkedNode extends SinglyLinkedNode {
   /**The next value attached to the linked list */
-  next?: DoublyLinkedNode<Type>;
+  next?: DoublyLinkedNode;
 
   /**The previous value attached to the linked list */
-  prev?: DoublyLinkedNode<Type>;
+  prev?: DoublyLinkedNode;
 
   /**Stores the values and data inside the list */
-  value: Type;
+  value: any;
 }
