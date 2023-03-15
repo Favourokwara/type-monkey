49385763f81e10c7c54e8e91fb6bb1c04a1cfd35
Favour Okwara <favourprogrammer@gmail.com>
2023-03-15 09:09:37 +0200

    declare next and previous node pointers

--- src/models/linked_node.ts
+++ src/models/linked_node.ts
@@ -2,12 +2,16 @@ import { SinglyLinkedNode, DoublyLinkedNode } from '../interfaces/linked_nodes';
 
 /**Data class used for building up and storing data in a singly linked list. */
 export class SinglyNode implements SinglyLinkedNode {
-  constructor(public value: any, public next?: SinglyNode) {}
+  public next?: SinglyNode;
+  constructor(public value: any) {}
 }
 
 /**Data class used for building up and storing data in a doubly linked list. */
 export class DoublyNode extends SinglyNode implements DoublyLinkedNode {
-  constructor(value: any, next?: DoublyNode, public prev?: DoublyNode) {
-    super(value, next);
+  public declare next?: DoublyNode;
+  public declare prev?: DoublyNode;
+
+  constructor(value: any) {
+    super(value);
   }
 }
