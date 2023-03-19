a96a9eccd836f3def7718a5ab044d71354f4990e
Favour Okwara <favourprogrammer@gmail.com>
2023-03-19 20:19:07 +0200

    add controller function

--- src/index.ts
+++ src/index.ts
@@ -3,75 +3,77 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-import { Letter } from "./models/letter";
-import { DoublyLinkedList } from "./models/linked_list";
+import { DoublyList } from './models/linked_list';
+import { DoublyNode } from './models/linked_node';
 
-// import confetti from 'canvas-confetti';
+/**Split a string into substrings by the end space boundaries and return them as an array.*/
+function stringToWords(s: String): String[] {
+  return s.split(/(?<=\s)/);
+}
 
-// confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-//   resize: true,
-//   useWorker: true,
-// })({ particleCount: 200, spread: 200 });
-
-function sentenceToDoublyLinkedList(sentence: string): DoublyLinkedList {
-  const words: string[] = sentence.split(/(?<=\s)/);
-  const doublyList = new DoublyLinkedList();
+/**Converts the provided string into a doubly linked list indexed by its words positions. */
+function stringToWordList(s: String): DoublyList {
+  const sentence = new DoublyList(),
+    words = stringToWords(s);
 
   for (let i = 0; i < words.length; i++) {
-    const wordLength = words[i].length;
     for (let j = 0; j < words[i].length; j++) {
-      doublyList.push(new Letter(words[i][j], j, i, wordLength));
+      sentence.push({ character: words[i][j], word: i, letter: j });
     }
-  } 
-  return doublyList
+  }
+  return sentence;
 }
 
+class TextController {
+  private reference: DoublyList = stringToWordList(
+    "something fishy is going on and i don't know what it is",
+  );
+  // private actual: DoublyList;
 
-class SentenceService {
-  public reference: DoublyLinkedList;
-  public typed_lst: DoublyLinkedList = new DoublyLinkedList();
-
-  constructor(ref: string) {
-    this.reference = sentenceToDoublyLinkedList(ref);
-  }
-  addCharacter(str: string) {
-    if (str.length == 1 && this.typed_lst.size < this.reference.size) {
-      const base: Letter = this.reference.getElementAt(this.typed_lst.size)?.getValue();
-      base.character = str;
-      this.typed_lst.push(base)
-    }
-  }
-  removeCharacter(): Letter {
-    return this.typed_lst.removeAt(this.typed_lst.size - 1);
-  }
-  removeWord(): Letter {
-    const tail = this.typed_lst.tail;
-    let current = tail
-    const moves = current?.getValue().letterIndex;
-    for (let i = 0; i < moves.length + 1 ; i++) {
-      current = current?.prev;
-    }
-    console.log(current);
-    
+  constructor(private view: TextView) {
+    view.render("something fishy is going on and i don't know what it is");
   }
 }
 
-const service = new SentenceService("this is what i do for a living.")
-service.addCharacter("t")
-service.addCharacter("h")
-service.addCharacter("i")
-service.addCharacter("s")
-service.addCharacter(" ")
-service.addCharacter("i")
-service.addCharacter("s")
-service.addCharacter(" ")
-service.addCharacter("w")
-service.addCharacter("h")
-service.addCharacter("a")
-service.addCharacter("t")
-service.addCharacter(" ")
-service.addCharacter("i")
+class TextView {
+  private sentence?: DoublyList;
+  private cursor?: DoublyNode;
 
-// console.log(service.removeWord())
-console.log(service.typed_lst);
+  constructor() {
+    this.sentence = new DoublyList();
+    const letters = document.getElementsByClassName('text-letter');
 
+    for (let i = 0; i < letters.length; i++) {
+      this.sentence.push(letters[i]);
+    }
+
+    this.cursor = this.sentence.head;
+    if (this.cursor) this.cursor.value.id = 'text-cursor';
+  }
+
+  incrementCursor() {
+    const current = this.cursor;
+    if (current != null) current.value.id = '';
+    this.cursor = current?.next;
+    if (this.cursor != null) this.cursor.value.id = 'text-cursor';
+  }
+
+  render(s: String) {
+    const textContainer = document.getElementById('text-cnt');
+    const sentence = new DocumentFragment(),
+      words = stringToWords(s);
+
+    for (let i = 0; i < words.length; i++) {
+      const word = document.createElement('span');
+      for (let j = 0; j < words[i].length; j++) {
+        const letter = document.createElement('span');
+        letter.innerHTML = words[i][j];
+        word.append(letter);
+      }
+      sentence.append(word);
+    }
+    textContainer?.appendChild(sentence);
+  }
+}
+
+const controller = new TextController(new TextView());
