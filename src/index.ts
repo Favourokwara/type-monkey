8f87280b1951c3eaedb1641b77b7d3e9d333aca1
Favour Okwara <favourprogrammer@gmail.com>
2023-03-24 21:09:39 +0200

    remove confetti canvas from index

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
 
 import { DoublyList } from "./models/linked_list";
 import type { DoublyNode } from "./models/linked_node";
 
-// import confetti from 'canvas-confetti';
-
-// confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-//   resize: true,
-//   useWorker: true,
-// })({ particleCount: 200, spread: 200 });
 
 class TextController {
     public textView: TextView;
